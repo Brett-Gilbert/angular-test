ab27bba9c9a11e90d67c51d6876b2265cefe303b
Brett Gilbert <bgilb13@gmail.com>
2017-02-15 10:48:28 -0700

    added home.php

---
+++ public_html/templates/home.php
@@ -0,0 +1,8 @@
+<section class="welcome">
+	<div class="container">
+		<div class="jumbotron text-center">
+			<h1>Welcome to the Angular 2 Test</h1>
+			<p class="lead">This is a bare-bones Angular 2 front end</p>
+		</div>
+	</div>
+</section>
